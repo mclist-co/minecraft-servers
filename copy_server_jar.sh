abf76d2b6e01175acd5ee925a402e512282ee28a
Paul Heidenreich <paul@paul-h.de>
2023-02-16 10:39:53 +0100

    Use multiarch base images where possible + More robust copy_server_jar script

--- copy_server_jar.sh
+++ copy_server_jar.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-export SETUP_ONLY=true
-/start
-export SETUP_ONLY=false
+while [ 1 ]; do
+  export SETUP_ONLY=true
+  /start
+  export SETUP_ONLY=false
 
-SERVER=$(find /data -name "*server*.jar")
+  SERVER=$(find /data -name "*server*.jar")
+
+  if [ -z "$SERVER" ]; then
+    echo "Server jar not found. Retrying in 5 seconds..."
+    sleep 5
+  else
+    break
+  fi
+done
+
 
 echo "Server jar found at $SERVER. Copying to /server.jar"
 
